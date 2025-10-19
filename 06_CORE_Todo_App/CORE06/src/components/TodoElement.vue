dcd66563cfb3345fba66c572d42f02f3b292d714
AlexBa16 <alexbachinger56@gmail.com>
2025-10-19 17:23:48 +0200

    CORE06

--- 06_CORE_Todo_App/CORE06/src/components/TodoElement.vue
+++ 06_CORE_Todo_App/CORE06/src/components/TodoElement.vue
@@ -1,33 +1,39 @@
-<script setup>
-
-const props = defineProps({
-  description: String,
-  timestamp: Date,
-  completed: Boolean,
-});
-
-const emit = defineEmits(["update:completed"]);
-
-function getAge(timestamp) {
-  const diff = new Date() - new Date(timestamp);
-  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
-  const minutes = Math.floor((diff / (1000 * 60)) % 60);
-  return days + 'd ' + hours + 'h ' + minutes + 'm';
-}
-
-function toggleCompleted() {
-  emit("update:completed", !props.completed);
-}
-</script>
-
 <template>
   <tr>
     <td>{{ description }}</td>
-    <td>{{ getAge(timestamp) }}</td>
+    <td>{{ getAge }}</td>
     <td class="complete" @click="toggleCompleted">{{ completed ? "✅" : "❌" }}</td>
   </tr>
 </template>
+
+<script>
+export default {
+  name: "TodoElement",
+
+  props: {
+    description: String,
+    timestamp: Number,
+    completed: Boolean,
+  },
+  
+  emits: ["update:completed"],
+
+  computed: {
+    getAge() {
+      const diff = new Date() - new Date(this.timestamp);
+      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((diff / (1000 * 60)) % 60);
+      return days + 'd ' + hours + 'h ' + minutes + 'm';
+    },
+  },
+  methods: {
+    toggleCompleted() {
+      this.$emit("update:completed", !this.completed);
+    },
+  },
+};
+</script>
 
 <style scoped>
 td.complete {
