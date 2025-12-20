67276a92010ef881dc007d5cca6bb6e9874436d2
AlexBa16 <alexbachinger56+GitHub@gmail.com>
2025-12-20 21:21:55 +0100

    add ADV7

---
+++ 07_ADV_Linting_Debuging/ADV07/src/components/TodoInput.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {ref} from "vue";
+
+const emit = defineEmits([]);
+const todoInput = ref("");
+
+function addTodo() {
+  if (!todoInput.value) return;
+  emit("add", todoInput.value);
+  todoInput.value = "";
+}
+</script>
+
+<template>
+  <input type="text" v-model.trim="todoInput" placeholder="Enter Todo">
+  <button type="button" @click="addTodo">Add</button>
+</template>
---
+++ 07_ADV_Linting_Debuging/ADV07/src/components/TodoList.vue
@@ -0,0 +1,29 @@
+<script setup>
+import TodoElement from './TodoElement.vue'
+
+defineProps({
+  todos: Array,
+})
+</script>
+
+<template>
+  <table v-if="todos.length" id="todos">
+    <thead>
+    <tr>
+      <th>ToDo</th>
+      <th>Created</th>
+      <th>Completed</th>
+    </tr>
+    </thead>
+    <tbody>
+    <TodoElement
+        v-for="item in todos"
+        :timestamp="item.timestamp"
+        :description="item.description"
+        :completed="item.completed"
+        @update:completed="(v) => item.completed = v"
+    />
+    </tbody>
+  </table>
+  <div v-else class="noTodos">No Todos!</div>
+</template>
