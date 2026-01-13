ac3585310b7c92218e0569f36aae779665dacc86
AlexBa16 <alexbachinger56+GitHub@gmail.com>
2026-01-13 16:21:45 +0100

    ADV14

--- 14_ADV_i18n/ADV14/src/i18n.ts
+++ 14_ADV_i18n/ADV14/src/i18n.ts
@@ -2,7 +2,7 @@ import {createI18n} from 'vue-i18n'
 import en from './locales/en.json'
 import de from './locales/de.json'
 
-const savedLang = localStorage.getItem('lang')
+const savedLang = localStorage.getItem('lang');
 const browserLang = navigator.language.split('-')[0]!;
 document.documentElement.lang = savedLang || (['en', 'de'].includes(browserLang) ? browserLang : 'en')!;
 
