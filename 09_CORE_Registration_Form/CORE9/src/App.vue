3924574568c3c9c14faeda480b50d125854b315e
AlexBa16 <alexbachinger56+GitHub@gmail.com>
2026-03-23 09:16:55 +0100

    finish CORE09

--- 09_CORE_Registration_Form/CORE9/src/App.vue
+++ 09_CORE_Registration_Form/CORE9/src/App.vue
@@ -1,36 +1,17 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import RegisterForm from "@/components/RegisterForm.vue";
 </script>
 
 <template>
-  <RegisterForm></RegisterForm>
+    <RegisterForm class="reg_form"></RegisterForm>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.reg_form {
+    height: 100vh;
+    max-width: 300px;
+    margin: auto;
+    align-content: center;
+    color: white;
 }
 </style>
